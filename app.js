749ea8ea7810502219d215065f685f936f148cf7
Prabhat <singhprabhatp.s.224@gamil.com>
2021-10-23 23:09:00 +0530

    Just footer is still remains

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var dateOfBirth = document.querySelector("#date-of-birth")
 var luckyNumber = document.querySelector("#lucky-number")
 var btnCheck = document.querySelector("#btn-check")
+var btnOutput = document.querySelector("#output")
 
 function addNumber(dob) {
     dob = dob.replaceAll("-", "")
     let sum = 0;
     for (let i = 0; i < dob.length; i++)
         sum = sum + Number(dob.charAt(i));
-    console.log(sum)
+    return sum
+}
+
+function isYourBirthdayLucky(dob, luckyNumber) {
+    if (dob % luckyNumber.value === 0)
+        btnOutput.innerText = "Yeah your Birthday is Lucky 🎉"
+    else if (dob && luckyNumber.value)
+        btnOutput.innerText = "Please Enter in the respected field"
+    else
+        btnOutput.innerText = "Bad Lucky , No Lucky Birthday 😞"
 }
 
 function EventHandler() {
-    addNumber(dateOfBirth.value)
-    console.log(`Clicked ${dateOfBirth.value} ${luckyNumber.value} `)
+    var dob = dateOfBirth.value;
+    var sum = addNumber(dob);
+
+    isYourBirthdayLucky(sum, luckyNumber)
+
 }
 
 btnCheck.addEventListener("click", EventHandler)
